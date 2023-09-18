446d927275ab9606196bf21a387f938d7133a997
jasmeet kaur <jasmeetkaur1723@gmail.com>
2023-09-19 02:10:05 +0530

    second commit some issue

---
+++ src/sections/index.js
@@ -0,0 +1,18 @@
+import Hero from './Hero';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+export{
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+};
